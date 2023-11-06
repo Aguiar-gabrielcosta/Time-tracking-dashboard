faf4bcda4dbdaecdd4cb9286fdead2e6151f79a7
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2023-11-06 18:53:34 -0300

    API Integration

--- src/APIConsumer.mjs
+++ src/APIConsumer.mjs
@@ -3,7 +3,6 @@ const url = 'http://localhost:3010/data'
 export default async function getData(){
     const response = await fetch(url)
     const data = await response.json()
-
-    console.log(data);
+    return data
 }
 
--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
-import getData from "./getAPIData.mjs";
 import { TimeMenu } from "./timeMenu.mjs";
 
-document.getElementById('daily').addEventListener('select', TimeMenu.dailyHours())
-document.getElementById("weekly").addEventListener('select', TimeMenu.weeklyHours())
-document.getElementById("monthly").addEventListener('select', TimeMenu.monthlyHours())
+
+// Start condition
+TimeMenu.dailyHours()
+
+
+
+// Listeners
+document.querySelector('input#daily').addEventListener('change', TimeMenu.dailyHours)
+document.querySelector('input#weekly').addEventListener('change', TimeMenu.weeklyHours)
+document.querySelector('input#monthly').addEventListener('change', TimeMenu.monthlyHours)
 
